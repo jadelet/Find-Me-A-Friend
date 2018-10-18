ea5c11e6e0a3b916bc86f29f6a791976c5a03894
Jadelet <slaethe@gmail.com>
2018-10-18 14:16:23 -0400

    added missing ajax request

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,24 +1,33 @@
+
 const friendsData = require("../app/data/friends");
 
-
-
+console.log (friendsData)
 
     module.exports = function (app) {
 
       app.get("/api/friends", function (req, res) {
-        res.json(friendData);
+        res.json(friends);
       });
 
       app.post("/api/friends", function (req, res) {
-
+ 
+          // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+          // It will do this by sending out the value "true" have a table
+          // req.body is available since we're using the body parsing middleware
+      
+           friendsData.push(req.body);
+            res.json(true);
     
 
         var newFriend = req.body;
 
         
-        friendData.push(newFriend);
+        friendsData.push(newFriend);
+        res.json(true);
+        console.log (friendsData);
 
-        friendAlgorithm (newFriend)
+        friendAlgorithm (newFriend);
+        res.end()
       });
     }
 
@@ -63,7 +72,3 @@ console.log (diffList)
           }
         }
       }
-
-          // This should have:
-          // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-          // * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
--- server.js
+++ server.js
@@ -14,23 +14,6 @@ app.use(express.json());
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
-// Create New Friend - takes in JSON input
-app.post("/api/Friend", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newFriend = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newfriend
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newFriend.routeName = newfriend.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newFriend);
-
-  friendsData.push(newFriend);
-
-  res.json(newFriend);
-});
-
 
 // switch on the server
 app.listen(PORT, function() {
